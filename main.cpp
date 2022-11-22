098d076472c331eb890e6afaecdb364660edd4e2
Alvbatross <jesus.pato@obf.ateneo.edu>
2022-11-22 14:39:48 +0800

    Added UIHandler.h

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <raylib.h>
 #include <raymath.h>
 
+#include <iostream>
+#include <string>
+#include <vector>
+
 #include "entt.hpp"
+#include "UIHandler.h"
 
 const int WINDOW_WIDTH(1280);
 const int WINDOW_HEIGHT(720);
@@ -9,7 +14,25 @@ const char* WINDOW_TITLE("⚔ Hack and Slash ⚔");
 
 int main() {
 	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
+	SetTargetFPS(60);
+	
+	MainMenu menu;
+
+	menu.createUI(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+
 	while (!WindowShouldClose()) {
 		// GAME
+		menu.Update();
+
+		ClearBackground(WHITE);
+        BeginDrawing();
+		menu.Draw();
+		EndDrawing();
 	}
+
+	CloseWindow();
+
+    return 0;
 }
+
